63ae22319a5141eb072fef866a6c6382fe529d0d
stephenZ22 <stephenzhao94@163.com>
2025-08-23 01:29:45 +0800

    use group dir

---
+++ app/(admin)/admin/page.tsx
@@ -0,0 +1,11 @@
+const Management = () => {
+  return (
+    <>
+      <main className="flex h-screen bg-gradient-to-br from-green-300 to-white">
+        <h1>Management</h1>
+      </main>
+    </>
+  );
+};
+
+export default Management;
