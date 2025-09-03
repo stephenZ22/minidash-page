f95db5e5fe16f0b93a4d5a05266bd257751c524c
stephenZ22 <stephenzhao94@163.com>
2025-09-03 21:41:39 +0800

    add mode toggle

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -2,15 +2,24 @@ import React from "react";
 
 import { SidebarProvider } from "@/components/ui/shadcn/sidebar";
 import { AppSidebar } from "@/components/pages/admin/AdminSidebar";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode_toggle";
 
 const Management = ({ children }: { children: React.ReactNode }) => {
   return (
     <SidebarProvider>
-      <div className="flex h-screen w-full">
-        <AppSidebar />
-
-        <main className="flex-1 p-8 overflow-y-auto">{children}</main>
-      </div>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <div className="flex h-screen w-full">
+          <AppSidebar />
+          <ModeToggle />
+          <main className="flex-1 p-8 overflow-y-auto">{children}</main>
+        </div>
+      </ThemeProvider>
     </SidebarProvider>
   );
 };
